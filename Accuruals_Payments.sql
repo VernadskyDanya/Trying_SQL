9784439071af3db894150d19540b57dd4ebe38cf
VernadskyDanya <d.vernadsky@yandex.ru>
2021-01-17 21:52:52 +0300

    chore: some changes towards idea of priority

--- Accuruals_Payments.sql
+++ Accuruals_Payments.sql
@@ -1,9 +1,13 @@
-SELECT * FROM
-(SELECT a.id 'id Долга', a.data 'a_data', a.month 'a.month', p.id 'id Платежа', p.data 'p_data', p.month 'p.month'
-FROM accrual as a LEFT JOIN payment as p
+WITH prom_Accrual (priority, id, data, month)
+AS
+(  
+	-- Расставляем приоритет 
+    SELECT row_number() over(PARTITION BY month ORDER BY data ASC) AS 'priority', id, data, month
+	FROM accrual
+    ORDER BY data
+)
+
+SELECT a.priority, a.id 'id Долга', a.data 'a_data', a.month 'a.month', p.id 'id Платежа', p.data 'p_data', p.month 'p.month'
+FROM prom_accrual as a RIGHT JOIN payment as p
 		ON a.month = p.month
-UNION
-SELECT a.id 'id Долга', a.data 'a_data', a.month 'a.month', p.id 'id Платежа', p.data 'p_data', p.month 'p.month'
-FROM accrual as a RIGHT JOIN payment as p
-		ON a.month = p.month) fullunion
-WHERE fullunion.a_data <= fullunion.p_data
+WHERE (a.data is NULL) OR (a.data <= p.data) AND (a.priority = 1) 
